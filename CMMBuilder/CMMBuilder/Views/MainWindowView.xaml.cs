e59a9b2ef28330e140bcfcd5d48fdceca2329094
snwmelt <ekitai.k@gmail.com>
2019-07-13 22:59:11 +0100

    MVVM Setup
    Directory restructure to MVVM.
    Moved MainWindow to MainWindowView.(cs/xaml).
    Added MVVM INotifyPropertyChanged and ICommand helpers.

---
+++ CMMBuilder/CMMBuilder/Views/MainWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CMMBuilder.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow( )
+        {
+            InitializeComponent( );
+        }
+    }
+}
