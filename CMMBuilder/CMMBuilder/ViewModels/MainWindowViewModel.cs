17c85d0c0a7b93ca95c4337aa8ec07c19e1dfa55
snwmelt <ekitai.k@gmail.com>
2019-07-13 23:08:44 +0100

    Implemented Main Window View Model
    Added MainWindowViewModel as ViewModel for MainWindow View.
    Added Binding for CurrentView to allow Population of MainWindow via it's view model.

---
+++ CMMBuilder/CMMBuilder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CMMBuilder.ViewModels.Helpers;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace CMMBuilder.ViewModels
+{
+    /// <summary>
+    /// View Model for MainWindowView. Provides UI Binding Resources and Behaviour.
+    /// </summary>
+    internal sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Private Properties
+        private readonly NotifyPropertyChangedInvoker _NPCInvoker;
+        private          Page                         _CurrentView;
+        #endregion
+
+        
+        public MainWindowViewModel( )
+        {
+            _NPCInvoker = new NotifyPropertyChangedInvoker( this );
+        }
+
+        /// <summary>
+        /// Current Page Displayed by MainWindowView Frame Element.
+        /// </summary>
+        public Page CurrentView
+        {
+            get
+            {
+                return _CurrentView;
+            }
+
+            set
+            {
+                _NPCInvoker.AssignPropertyValue( ref PropertyChanged, ref _CurrentView, value );
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
