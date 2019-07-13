ba876fd542774743b3caf3f90e412df633d548dd
snwmelt <ekitai.k@gmail.com>
2019-07-13 23:30:18 +0100

    CMMBuilderPage View Model
    Added CMMBuilderPageViewModel as View Model for CMMBuilderPage View.
    Bound CMMBuilderPage datacontext to CMMBuilderPageViewModel instance.
    Added Content Binding for dynamic elements in CMMBuilderPage View.

---
+++ CMMBuilder/CMMBuilder/ViewModels/CMMBuilderPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CMMBuilder.ViewModels.Helpers;
+using System.ComponentModel;
+
+namespace CMMBuilder.ViewModels
+{
+    internal sealed class CMMBuilderPageViewModel : INotifyPropertyChanged
+    {
+        private NotifyPropertyChangedInvoker _NPCInvoker;
+
+        public CMMBuilderPageViewModel()
+        {
+            _NPCInvoker = new NotifyPropertyChangedInvoker( this );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
